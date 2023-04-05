f550e7ef85f63c7e9ee7d8985b40b58817d7ff14
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-05 20:14:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ handler = WebhookHandler('82ab0090dc70c5f7d3a6c62fb1e09eb8')
 line_user_id = ''
 
 # 監聽所有來自 /callback 的 Post Request
-@app.route("/r4u009", methods=['POST'])
+@app.route("/r4u010", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
    
