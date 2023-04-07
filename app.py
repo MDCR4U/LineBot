a7dcb93dfdfc7f96751551f4558ac4b9003d85a1
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-07 13:34:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -80,10 +80,10 @@ line = file.readline().strip('\n')   #line1 githubproject
 #line=line.strip('\n')
 github_prj = line[12:].strip()
 file.close()
- 
+print ("=====================================\n" + github_id +"\n" + github_prj + "\n" + githuburl + "\n======================") 
 
 githuburl="https://github.com/" + github_id + "/" + github_prj + "blob/main/"
-print ("=====================================\n" + github_id +"\n" + github_prj + "\n" + githuburl + "\n======================")
+
 #取得 系統 KEY 
 url = githuburl + "key.txt"
 print("========================= " + url )
