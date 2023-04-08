c9dccf87b3a583f77320dc5651a189d688a2ece0
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-08 17:33:16 +0800

    Add files via upload

--- sendmails.py
+++ sendmails.py
@@ -32,7 +32,7 @@ def send_mail(lineid,wmsg,userFolder):
 
 
     #print("\n LINEID " + lineid)
-    wstarget = wmsg[6:]
+    wstarget = wmsg[7:]
     print("wstarget :" + wstarget +"*")
     if (wstarget.isdigit()):
         targetno = int(wstarget)
@@ -40,7 +40,7 @@ def send_mail(lineid,wmsg,userFolder):
     else : 
         targetno = 0
     #        print("invalid target no ")
-        return("發送信件格式 錯誤\n正確格式==>/SMAIL:nnnn\n 結束作業 :" + wstarget +"*")   
+        return("發送信件格式 錯誤\n正確格式==>/SMAIL:nnnn\n 結束作業 :*" + wstarget +"*")   
      
     print("LINE @ id = " + lineid)
     wssts = check_line_id(wsftpflr,lineid)
