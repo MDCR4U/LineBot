ffe02ac06efe220d87fc1b560de30e17201da9f9
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-10 10:48:27 +0800

    Add files via upload

--- message.py
+++ message.py
@@ -15,6 +15,7 @@ import urllib.request
 
 #ImagemapSendMessage(組圖訊息)
 def imagemap_5_message(msg):
+    print ("process image * 5")
     #wsmsg = msg.split('#')
     wsmsg = "myjson" 
     wjson_file = wsmsg + ".json"
@@ -22,7 +23,8 @@ def imagemap_5_message(msg):
     with open("cbd.json" , "r") as f:
         js_dta = json.load(f)
  
-
+    print ("get cbd7.json  complete")
+    print ("background " + js_dta["background_url"] )
     background_url  = js_dta["background_url"]                           #"https://i.ibb.co/mJfp6Nf/background.png"  #https://ibb.co/0BZHztf"
     url_top_left    = js_dta["url_top_left"]                             #"https://www.youtube.com/watch?v=0kOpOqHuiGo"
     url_top_right   = js_dta["url_top_right"]                            #"https://www.youtube.com/watch?v=XFvgYYHvcfE"
