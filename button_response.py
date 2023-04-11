0079c7920c5afb8e46d37f2c286e561f12ae5aff
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-11 15:14:05 +0800

    Add files via upload

--- button_response.py
+++ button_response.py
@@ -15,7 +15,7 @@ import urllib.request
 #                                        text='台北市',
 #                                        data='A&台北市'
 #                                    ),
-def buttons_31(msg):    # 3 text, 1URL
+def buttons_41(msg):    # 3 text, 1URL
     print (" process 3 text 1url button")
     message = TemplateSendMessage(
         alt_text='CBD的法律常識～',
