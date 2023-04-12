e7d3089d6608501c8fd98a8dd890a255d2230d21
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-12 17:40:48 +0800

    Add files via upload

--- button_response.py
+++ button_response.py
@@ -508,7 +508,71 @@ def buttons_31(msg):     # 3 text, 1URL
 
     print(message)
     return message
-def buttons_40(msg):    # 4 text, 0URL
+def buttons_40(msg):     # 3 text, 1URL   
+    print (" process 3 text 1url button " + msg)
+    file = open('config.txt','r',encoding="utf-8")
+    line = file.readline().strip('\n')    #line1 githubid
+    line = file.readline().strip('\n')   #line1 githubproject
+    line = file.readline().strip('\n')   #line1 githubproject
+    #line=line.strip('\n')
+    wsftpflr= line[12:].strip()
+
+    wsmsg = msg.split('#')
+    
+    wjson_file = wsmsg[1] + ".json"
+# 讀取 JSON 檔案  local
+#   # with open("cbd.json" , "r") as f:
+    #with open(wjson_file , "r") as f:
+    #    js_dta = json.load(f)
+
+    url = wsftpflr + "json/" + wjson_file #http://www.abc.com/cust.json"
+    print(url)
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+    js_dta = json.loads(data)
+    image_url  = js_dta["image"]   
+    alt_text   = js_dta["alt_text"]
+    title      = js_dta["title"]
+    text0      = js_dta["text0"]
+    label1     = js_dta["label1"]
+    label2     = js_dta["label2"]
+    label3     = js_dta["label3"]
+    label4     = js_dta["label4"]
+    url1       = js_dta["url1"]
+    text1      = js_dta["text1"]
+    text2      = js_dta["text2"]
+    text3      = js_dta["text3"]
+    text4      = js_dta["text4"]
+    message = TemplateSendMessage(
+        alt_text= alt_text ,   #'CBD的法律常識～',
+        template=ButtonsTemplate(
+            thumbnail_image_url= image_url, #"https://i.ibb.co/NWrhxmc/cbd.jpg",
+            title= title,      #CBD的百寶庫",
+            text=text0,       #"選擇您想要的內容",
+            actions=[
+                MessageTemplateAction(
+                    label=label1   , #"認識CBD影片介紹",
+                    text=text1  ,   #"https://www.youtube.com/watch?v=0kOpOqHuiGo"
+                ),
+                MessageTemplateAction(
+                    label= label2 ,  #"CBD的法律常識",
+                    text=text2 ,    #"/C20"
+                ),
+                MessageTemplateAction(
+                    label=label3  ,   #"CBD的研究報告",
+                    text=text3  ,   #"/C30"
+                ),
+                MessageTemplateAction(
+                    label=label4 ,   #"更多選項",
+                    text=text4
+                )
+            ]
+        )
+    )
+
+    print(message)
+    return message
+def xbuttons_40(msg):    # 4 text, 0URL
     print (" process 4 text 0 url button")
     file = open('config.txt','r',encoding="utf-8")
     line = file.readline().strip('\n')    #line1 githubid
