2fb8c22453238e551e930c00c68e2ab6a7d489c4
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-17 22:49:00 +0800

    Add files via upload

--- sendmails.py
+++ sendmails.py
@@ -310,6 +310,8 @@ def loadfile(lineid,msg,userFolder ):
     #filename = wsflr + "_" + wmsg[2]
     filename = wsflr + "/" + wmsg[2]
     print ("source from : " + url  + " to: " + filename ) 
+
+    return
    #url 是要下載的檔案的 URL，
    # file_name 則是下載後要儲存的檔案名稱和路徑
    # （如果只指定檔案名稱，則預設儲存到目前的資料夾中）。 urlretrieve() 函式會從指定的 URL 下載檔案，並將其儲存在 file_name 指定的位置。   
