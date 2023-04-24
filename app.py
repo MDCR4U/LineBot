044ca78c400784c6bac90aa8b48264bdaa6112a4
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-24 20:57:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -119,11 +119,18 @@ gpt_token           = loaded_data["gptkey"]
 
 print ("line_access_token " + line_access_token )
 print ("line_channel_secret " + line_channel_secret)
+print("ftpurl " + ftpurl)
+
+userFolder = check_line_id(ftpurl ,line_user_id)
+
 
 # Channel Access Token 
 line_bot_api = LineBotApi(line_access_token)
 # Channel Secret
 handler = WebhookHandler(line_channel_secret)
+
+
+
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/rich4u_004", methods=['POST'])
@@ -147,7 +154,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    userFolder = ''
+    
     usr =event.source.user_id
     line_user_id = usr
     print("ftpurl " + ftpurl)
