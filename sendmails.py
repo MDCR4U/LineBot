6b79bcdfc6ecb75cf5b35bba04bc02598144400f
MDCR4U <128909871+MDCR4U@users.noreply.github.com>
2023-04-25 14:14:49 +0800

    Add files via upload

--- sendmails.py
+++ sendmails.py
@@ -113,7 +113,7 @@ def send_mail(lineid,wmsg,userFolder):
         print ("寄件者資料 讀取錯誤 \n " + url)
         return ("寄件者資料 讀取錯誤 \n " + url)
 
-    print("發信者 人數" + len(smtp_list))
+    print("發信者 人數" + str(len(smtp_list)))
 # 讀取郵件發送記錄
     #url = wsftpflr + userFolder.strip('\n') +  '_mail_counter.log'
     #try:
